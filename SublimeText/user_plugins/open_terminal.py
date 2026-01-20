87eecd62996c1d4b77202f4fabf350d128685c23
mortalis13 <stangerroman@gmail.com>
2026-01-20 14:51:48 +0100

    open terminal command

---
+++ SublimeText/user_plugins/open_terminal.py
@@ -0,0 +1,21 @@
+# Opens the project path in a terminal
+# Examples:
+# {"keys": ["f10"], "command": "open_terminal", "args": {"cmd": "cmd.exe"}},
+# {"keys": ["ctrl+f10"], "command": "open_terminal", "args": {"cmd": "c:/windows-terminal/wt.exe --window last new-tab -d"}},
+
+import sublime
+import sublime_plugin
+
+import os
+import shlex
+import subprocess
+
+class OpenTerminalCommand(sublime_plugin.WindowCommand):
+    def run(self, cmd):
+        data = self.window.project_data()
+        project_path = data['folders'][0]['path']
+        project_path = project_path.replace('\\', '/')
+
+        cmd = f'{cmd} {project_path}'
+        args = shlex.split(cmd)
+        subprocess.Popen(args, cwd=project_path)
